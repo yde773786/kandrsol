48699f56327c7c1437e1dc8f5d1eeeb2218e6ba3
yde773786 <63968183+yde773786@users.noreply.github.com>
2020-06-14 12:37:54 +0400

    Create converter.c

---
+++ converter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+
+double convert(int farenheit);
+
+main()
+{
+    int fahr , celsius;
+    for(fahr = UPPER ; fahr >=LOWER ; fahr = fahr - 20)
+    {
+	printf("%3d %6.1f\n", fahr , convert(fahr));
+    }
+}
+
+double convert(int farenheit)
+{
+    return (5.0/9.0)*(farenheit-32);
+}
